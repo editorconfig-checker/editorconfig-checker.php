f951c45741940fb5464848b214a98a300399c7be
Max Strübing <mxstrbng@gmail.com>
2017-01-25 06:41:29 +0100

    TASK: Get the files from param and editorconfig rules for ft

--- src/MStruebing/EditorconfigChecker/Cli/Cli.php
+++ src/MStruebing/EditorconfigChecker/Cli/Cli.php
@@ -4,17 +4,81 @@ namespace MStruebing\EditorconfigChecker\Cli;
 
 class Cli
 {
+
+    const DEFAULT_INDENT_STYLE = 'tab';
+
     public function run($argv)
     {
-        if (count($argv) == 1) {
+        // We need files to check
+        if (count($argv) === 0) {
             $this->printUsage();
             return;
         }
 
         $rootDir = getcwd();
+        $editorconfigPath = $rootDir . '/.editorconfig';
 
-        $editorconfig = parse_ini_file($rootDir . '/.editorconfig', true);
-        var_dump($editorconfig);
+        if (is_file($editorconfigPath)) {
+            $editorconfig = parse_ini_file($rootDir . '/.editorconfig', true);
+        } else {
+            throw new \Exception('ERROR: No .editorconfig found');
+        }
+
+        $files = $this->getFiles($argv);
+
+        /* var_dump($files); */
+        /* var_dump($editorconfig); */
+        $this->checkFiles($editorconfig, $files);
+    }
+
+    protected function checkFiles($editorconfig, $files)
+    {
+        foreach ($files as $file) {
+            /* @TODO flatten the array before */
+            /* @TODO do it the other way around -> iterate over editorconfig */
+            if (isset($file[0])) {
+                $rules = $this->getRulesForFiletype($editorconfig, $file[0]);
+                var_dump($rules);
+            }
+        }
+    }
+
+    protected function getRulesForFiletype($editorconfig, $file)
+    {
+        $fileType = pathinfo($file, PATHINFO_EXTENSION);
+        $ftRules = $this->getEditorconfigRules($editorconfig, $fileType);
+
+        if ($ftRules !== false) {
+            $rules = $ftRules;
+        }
+
+        if (!isset($rules['indent_type'])) {
+            $rules['indent_type'] = self::DEFAULT_INDENT_STYLE;
+        }
+
+        return $rules;
+    }
+
+    protected function getEditorconfigRules($editorconfig, $fileType)
+    {
+        $rules = false;
+
+        foreach ($editorconfig as $key => $value) {
+            if (strpos($key, $fileType) !== false) {
+                $rules = $value;
+            }
+        }
+
+        return $rules;
+    }
+
+    protected function getFiles($fileGlobs)
+    {
+        $files = array();
+        foreach ($fileGlobs as $fileGlob) {
+            array_push($files, glob($fileGlob, GLOB_BRACE));
+        }
+        return $files;
     }
 
     protected function printUsage()
--- src/MStruebing/EditorconfigChecker/EditorconfigChecker.php
+++ src/MStruebing/EditorconfigChecker/EditorconfigChecker.php
@@ -8,7 +8,11 @@ $cliPath = dirname(__FILE__) . '/Cli/Cli.php';
 
 if (is_file($cliPath)) {
     include_once $cliPath;
+} else {
+    throw new \Exception('The CLI-script can not be found. Contact the maintainer.');
 }
 
 $cli = new Cli();
+// The first paaram is only the program-name
+array_shift($argv);
 $cli->run($argv);
