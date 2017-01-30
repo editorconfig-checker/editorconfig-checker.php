c509b94abb7e673cb218a70a4ea8b4efb155b71d
Max Strübing <mxstrbng@gmail.com>
2017-01-30 06:18:22 +0100

    TASK: Format logging output

--- src/MStruebing/EditorconfigChecker/Cli/Logger.php
+++ src/MStruebing/EditorconfigChecker/Cli/Logger.php
@@ -8,14 +8,21 @@ class Logger
 
     public function addError($message, $file = null, $lineNumber = null)
     {
-        array_push($this->errors, [$lineNumber, $file, $message]);
+        array_push($this->errors, ["lineNumber" => $lineNumber, "file" => $file, "message" => $message]);
     }
 
     public function printErrors()
     {
         foreach ($this->errors as $errorNumber => $error) {
-            var_dump($error);
+            printf("Error #%d" . PHP_EOL, $errorNumber);
+            printf("\t %s" . PHP_EOL, $error['message']);
+            printf("\t on line %d" . PHP_EOL, $error['lineNumber']);
+            printf("\t in file %s" . PHP_EOL, $error['file']);
+            printf(PHP_EOL);
         }
+
+        printf('%d errors occured' . PHP_EOL, $this->countErrors());
+        printf('Check log above and fix the issues.' . PHP_EOL);
     }
 
     public function countErrors()
