37233dd939c29caa71aa024d4cb0ec59fd0684d3
Max Strübing <struebing@sitegeist.de>
2017-02-03 15:28:59 +0100

    TASK: Add correct array shifting

--- src/MStruebing/EditorconfigChecker/EditorconfigChecker.php
+++ src/MStruebing/EditorconfigChecker/EditorconfigChecker.php
@@ -23,22 +23,19 @@ if (is_file($loggerPath)) {
 /* The first paaram is only the program-name */
 array_shift($argv);
 
-var_dump($argv);
-
-$optind = null;
-
 $shortOpts = 'hde:';
 $longOpts  = ['help', 'dots', 'exclude:'];
 $options = getopt($shortOpts, $longOpts);
 
 foreach ($options as $option) {
+    if ($option) {
+        array_shift($argv);
+    }
+
     array_shift($argv);
 }
 
-/* $filePattern = array_slice($argv, count($options)); */
-
 var_dump($options);
-/* var_dump($filePattern); */
 var_dump($argv);
 
 die;
