6a88681f20ad56eb912a98c179651ccef74f9dc4
Max Strübing <mxstrbng@gmail.com>
2017-05-04 08:16:45 +0200

    TASK: Add additional test for Editorconfig

--- tests/Editorconfig/EditorconfigTest.php
+++ tests/Editorconfig/EditorconfigTest.php
@@ -32,4 +32,22 @@ final class EditorconfigTest extends TestCase
         $rules = $editorconfig->getRulesForFile($allRules, 'Readme.md');
         $this->assertEquals($expectedRules, $rules);
     }
+
+    public function testOverwriteOfRules()
+    {
+        $allRules = $this->getRulesArray('Build/TestFiles/.editorconfig');
+
+        $expectedRules = [
+            'end_of_line' => 'lf',
+            'insert_final_newline' => 1,
+            'charset' => 'utf-8',
+            'trim_trailing_whitespace' => 1,
+            'indent_style' => 'space',
+            'indent_size' => 4
+        ];
+
+        $editorconfig = new Editorconfig();
+        $rules = $editorconfig->getRulesForFile($allRules, 'Readme.md');
+        $this->assertEquals($expectedRules, $rules);
+    }
 }
