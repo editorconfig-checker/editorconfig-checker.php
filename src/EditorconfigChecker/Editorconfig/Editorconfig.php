e26e97ea043b9135f9e23de9a084b1e8024d4e60
Max Strübing <mxstrbng@gmail.com>
2017-06-04 18:51:40 +0200

    FEATURE: Read editorconfig recursively until root=true

--- src/EditorconfigChecker/Editorconfig/Editorconfig.php
+++ src/EditorconfigChecker/Editorconfig/Editorconfig.php
@@ -7,41 +7,56 @@ use Webmozart\PathUtil\Path;
 class Editorconfig
 {
     /**
-     * Returns the editorconfig as an array
+     * Returns the editorconfig of a given path as an array
      *
      * @param string $editorconfigPath
      * @return array
      */
-    public function getRulesAsArray($editorconfigPath)
+    protected function getRulesAsArray($editorconfigPath)
     {
         return parse_ini_file($editorconfigPath, true);
     }
 
-    protected function getNearestEditorconfigRules($baseDir)
+    protected function getNearestEditorconfigRulesAsArray($baseDir)
     {
         $baseEditorconfig = $baseDir . '/.editorconfig';
         if (is_file($baseEditorconfig)) {
             return $this->getRulesAsArray($baseEditorconfig);
         } else {
-            return $this->getNearestEditorconfigRules(dirname($baseDir));
+            return $this->getNearestEditorconfigRulesAsArray(dirname($baseDir));
         }
     }
 
+    protected function mergeRulesForFile($fileName)
+    {
+        $editorconfig = $this->getNearestEditorconfigRulesAsArray(getcwd() . pathinfo(substr($fileName, 1))['dirname']);
+
+        return array_reduce(array_keys($editorconfig), function ($carry, $pattern) use ($editorconfig, $fileName) {
+            $rules = $editorconfig[$pattern];
+            $fileName = substr($fileName, 2);
+
+            return $pattern === 'root' ? ['root' => $editorconfig[$pattern]] : (
+                Glob::match(sprintf('%s/%s', getcwd(), $fileName), Path::makeAbsolute('**/' . $pattern, getcwd())) ?
+                    array_merge($carry, $rules) : $carry
+            );
+        }, []);
+    }
+
     /**
-     * Returns the editorconfig rules for a file
+     * Returns the editorconfig rules for a given file
      *
      * @param string $fileName
      * @return array
      */
     public function getRulesForFile($fileName)
     {
-        $editorconfig = $this->getNearestEditorconfigRules(getcwd() . pathinfo(substr($fileName, 1))['dirname']);
+        $rules = [];
 
-        return array_reduce(array_keys($editorconfig), function ($carry, $pattern) use ($editorconfig, $fileName) {
-            $rules = $editorconfig[$pattern];
+        do {
+            $rules = array_merge($this->mergeRulesForFile($fileName), $rules);
+            $fileName = dirname(pathinfo($fileName)['dirname']) . pathinfo($fileName)['basename'];
+        } while (!isset($rules['root']));
 
-            return Glob::match(sprintf('%s/%s', getcwd(), $fileName), Path::makeAbsolute('**/' . $pattern, getcwd())) ?
-                array_merge($carry, $rules) : $carry;
-        }, []);
+        return $rules;
     }
 }
