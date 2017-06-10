6c01fc96726a5663b41637155d9a0a5a27710f5a
Max Strübing <mxstrbng@gmail.com>
2017-06-10 14:19:09 +0200

    TASK: Refactor recursive search of editorconfig

--- src/EditorconfigChecker/Editorconfig/Editorconfig.php
+++ src/EditorconfigChecker/Editorconfig/Editorconfig.php
@@ -18,32 +18,15 @@ class Editorconfig
     }
 
     /**
-     * Get the nearest editorconfig to the given path
-     *
-     * @param string $baseDir
-     * @return array
-     */
-    protected function getNearestEditorconfigRulesAsArray($baseDir)
-    {
-        $baseEditorconfig = $baseDir . '/.editorconfig';
-
-        if (is_file($baseEditorconfig)) {
-            return $this->getRulesAsArray($baseEditorconfig);
-        } else {
-            return $this->getNearestEditorconfigRulesAsArray(dirname($baseDir));
-        }
-    }
-
-    /**
      * Merge editorconfig rules for a given file
+     * from an array of editorconfig rules
      *
      * @param string $fileName
+     * @param array $editorconfig
      * @return array
      */
-    protected function mergeRulesForFile($fileName)
+    protected function mergeRulesForFile($fileName, $editorconfig)
     {
-        $editorconfig = $this->getNearestEditorconfigRulesAsArray(getcwd() . pathinfo(substr($fileName, 1))['dirname']);
-
         return array_reduce(array_keys($editorconfig), function ($carry, $pattern) use ($editorconfig, $fileName) {
             $rules = $editorconfig[$pattern];
             $fileName = substr($fileName, 2);
@@ -56,20 +39,42 @@ class Editorconfig
     }
 
     /**
+     * Return the nearest matching editorconfig rules
+     *
+     * @param string $filePath
+     * @param string $fileBasename
+     * @param string $rootDir
+     */
+    protected function getNearestMatchingEditorconfigRules($filePath, $fileBasename, $rootDir)
+    {
+        $currentPath = $filePath;
+        $editorconfig = null;
+
+        do {
+            if (is_file($currentPath . '/.editorconfig')) {
+                $editorconfig = $this->getRulesAsArray($currentPath . '/.editorconfig');
+                $editorconfig = $this->mergeRulesForFile($fileBasename, $editorconfig);
+            }
+            $currentPath = dirname($currentPath);
+        } while (strpos($currentPath, $rootDir) === 0 && sizeof($editorconfig) === 0);
+
+        return $editorconfig;
+    }
+
+    /**
      * Returns the editorconfig rules for a given file
-     * until root=true
      *
      * @param string $fileName
      * @return array
      */
     public function getRulesForFile($fileName)
     {
-        $rules = [];
+        $pathinfo = pathinfo(getcwd() . substr($fileName, 1));
+        $rootDir = getcwd();
+        $filePath = $pathinfo['dirname'];
+        $fileBasename = $pathinfo['basename'];
 
-        do {
-            $rules = array_merge($this->mergeRulesForFile($fileName), $rules);
-            $fileName = dirname(pathinfo($fileName)['dirname']) . pathinfo($fileName)['basename'];
-        } while (!isset($rules['root']));
+        $rules = $this->getNearestMatchingEditorconfigRules($filePath, $fileBasename, $rootDir);
 
         return $rules;
     }
